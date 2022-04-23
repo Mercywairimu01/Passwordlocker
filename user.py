d47d43a6c407475b0ed4b8772fc5dea6c8ef7287
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-04-23 19:27:13 +0300

    add an empty users list and add a function to save a newly created user instance

--- user.py
+++ user.py
@@ -1,12 +1,19 @@
-from typing_extensions import Self
-
 
 class User:
     """
     Class that generates new instance of users.
-    """
+    """  
+    user_list = []   #empty users list
     
     def __init__(self, user_name, password):
         self.user_name = user_name
         self.password = password
-    
+        
+    def save_user(self):
+        '''
+        Function to save a newly created user instance
+        '''
+        User.user_list.append(self)
+  
+  
+   

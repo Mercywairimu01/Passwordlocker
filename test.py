1bdbbec2549e06be31fdc6d6c1f44b3bb64f19e0
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-04-24 11:22:52 +0300

    add importations for the unittest, the User and Credentials class

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest #import the unittest module
+from user import User #import the User class
+from credentials import Credentials #import the Credentials class
+
+class TestUser(unittest.TestCase):
+    """
+    A test class for test cases of the User class
+    """
+    

7392028b720bf46be3cd6e623d379586bc2f50a7
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-04-24 11:13:37 +0300

    add an instance to save the created credentials

--- credentials.py
+++ credentials.py
@@ -4,6 +4,14 @@ class Credentials:
     """
     Credentials_array= [] #empty credentials list
     def __init__(self,account_name,account_password):
+        """
+        define the properties of the credentials object
+        """
         self.account_name =account_name
         self.account_password =account_password
-       
+        
+    def save_credentials(self):
+        """
+        saves a new credential instance
+        """   
+        Credentials.Credentials_array.append(self) 

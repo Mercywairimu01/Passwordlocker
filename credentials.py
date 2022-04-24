de66e50131e59e61b53dd25d1f025e2af6a64757
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-04-24 10:55:32 +0300

    add an init method to generate a new instance of a credential

---
+++ credentials.py
@@ -0,0 +1,9 @@
+class Credentials:
+    """
+    Class for credentials
+    """
+    Credentials_array= [] #empty credentials list
+    def __init__(self,account_name,account_password):
+        self.account_name =account_name
+        self.account_password =account_password
+       
